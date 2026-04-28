55ad348d3e9a20a615b0fbdb2225a8c5bd5afad3
Anushka Prajapati <anushkaprajapati9898@gmail.com>
2026-04-28 18:39:16 +0530

    Add files via upload

---
+++ explicit.py.py
@@ -0,0 +1,9 @@
+# take num as input , convert to float and print both original and converted
+#  value with their data types
+
+num = input("Enter a value :")
+convertedvalue = float(num)
+
+print("Original value is",num,"Data Type :",type(num))
+
+print("Converted value is ",convertedvalue,"Data Type :",type(convertedvalue))
---
+++ indexing.py.py
@@ -0,0 +1,5 @@
+str1 = "Anushka"
+length = len(str1) 
+print(length) #7
+print(str1[0]) #A
+print(str1[4]) #h
